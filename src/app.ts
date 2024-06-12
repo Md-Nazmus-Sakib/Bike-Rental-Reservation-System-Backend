deac19bd5f6a812f4ac0de44440dcfa032904077
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2024-06-12 08:14:00 +0600

    global-error-handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 
 import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 const app: Application = express();
 
 //parsers
@@ -14,5 +15,8 @@ app.use('/api', router);
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+// Global Error Handel
+app.use(globalErrorHandler);
 
 export default app;
