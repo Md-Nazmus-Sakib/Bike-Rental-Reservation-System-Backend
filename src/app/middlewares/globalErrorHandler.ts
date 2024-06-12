a69451fd3d2121a7a045d9f9e5360f0cdd854566
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2024-06-12 12:09:26 +0600

    handle-zod-error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,17 +1,36 @@
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import handleZodError from '../errors/handeleZodError';
+import { TErrorSources } from '../interface/error';
+import config from '../config';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = 500;
-  const message = err.message || 'Something Went Wrong !';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // Default Value
+  let statusCode = 500;
+  let message = 'Something Went Wrong !';
+
+  // default message
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something Went Wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+
+  //Final Return is below
+
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
