10e32fb7aa0b9ef8ec6c64a6678438766ad0f7c0
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2024-06-12 08:42:44 +0600

    create-user-validation

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { AuthController } from './auth.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import createUserValidationSchema from '../Users/user.validation';
 
 const router = Router();
 
 router.post(
   '/signup',
-
+  validateRequest(createUserValidationSchema),
   AuthController.createUser,
 );
 
--- src/app/modules/Users/user.validation.ts
+++ src/app/modules/Users/user.validation.ts
@@ -1,27 +1,27 @@
 import { z } from 'zod';
 
-const userValidationSchema = z.object({
-  name: z.string().min(1, { message: 'Name is required' }).trim(),
-  email: z
-    .string()
-    .min(1, { message: 'Email is required' })
-    .email({ message: 'Please enter a valid email address' })
-    .toLowerCase()
-    .trim(),
-  password: z.string().min(1, { message: 'Password is required' }),
+const createUserValidationSchema = z.object({
+  body: z.object({
+    name: z.string().min(1, { message: 'Name is required' }).trim(),
+    email: z
+      .string()
+      .min(1, { message: 'Email is required' })
+      .email({ message: 'Please enter a valid email address' })
+      .toLowerCase()
+      .trim(),
+    password: z.string().min(1, { message: 'Password is required' }),
 
-  phone: z
-    .string()
-    .min(1, { message: 'Phone number is required' })
-    .regex(/^\+?[1-9]\d{1,14}$/, {
-      message: 'Please enter a valid phone number',
+    phone: z
+      .string()
+      .min(1, { message: 'Phone number is required' })
+      .regex(/^\+?[1-9]\d{1,14}$/, {
+        message: 'Please enter a valid phone number',
+      }),
+    address: z.string().min(1, { message: 'Address is required' }),
+    role: z.enum(['admin', 'user'], {
+      message: 'Role must be either admin or user',
     }),
-  address: z.string().min(1, { message: 'Address is required' }),
-  role: z.enum(['admin', 'user'], {
-    message: 'Role must be either admin or user',
   }),
 });
 
-export const UserValidation = {
-  userValidationSchema,
-};
+export default createUserValidationSchema;
