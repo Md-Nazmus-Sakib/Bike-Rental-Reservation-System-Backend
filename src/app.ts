b1a4ab642bf893af11e0daed4057130d14b68efe
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2024-06-12 08:30:56 +0600

    api-not-found

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Application, Request, Response } from 'express';
 
 import router from './app/routes';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 const app: Application = express();
 
 //parsers
@@ -18,5 +19,8 @@ app.get('/', (req: Request, res: Response) => {
 
 // Global Error Handel
 app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
 
 export default app;
