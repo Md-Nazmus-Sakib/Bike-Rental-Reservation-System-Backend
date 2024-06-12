796085c291d2789091067820ec9063fddc31a40d
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2024-06-12 13:55:59 +0600

    app-error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -6,6 +6,7 @@ import config from '../config';
 import handleValidationError from '../errors/handleValidationError';
 import handleCastError from '../errors/handleCastError';
 import handleDuplicateError from '../errors/handleDuplicateError';
+import AppError from '../errors/AppErrors';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   // Default Value
@@ -41,13 +42,37 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorSources = simplifiedError?.errorSources;
   }
 
-  //Duplicate Error
+  // Already Exist Error
   else if (err?.code === 11000) {
     const simplifiedError = handleDuplicateError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
   }
+
+  //AppError Handler
+  else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+  }
+
+  //Ultimate error handler
+  else if (err instanceof Error) {
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+  }
+
   //Final Return is below
 
   return res.status(statusCode).json({
