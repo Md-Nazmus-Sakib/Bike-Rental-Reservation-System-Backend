e12746a6409cd04a0cb48f4f57ec02cb3f14cd96
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2024-06-13 13:26:34 +0600

    booking-interface

--- src/app/modules/Bike/bike.service.ts
+++ src/app/modules/Bike/bike.service.ts
@@ -1,3 +1,5 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppErrors';
 import { TBike } from './bike.interface';
 import { Bike } from './bike.model';
 
@@ -6,11 +8,19 @@ const createBikeIntoDB = async (bikeData: TBike) => {
   return result;
 };
 const getAllBikeFromDB = async () => {
-  const result = await Bike.find();
+  const result = await Bike.find({ isAvailable: true });
   return result;
 };
 
 const updateBikeInfoIntoDB = async (id: string, payload: Partial<TBike>) => {
+  // Find the bike by its ID
+  const bike = await Bike.findById(id);
+
+  // Check if the bike is found
+  if (!bike) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Bike not found');
+  }
+
   const result = await Bike.findByIdAndUpdate(
     id,
     { $set: payload },
@@ -21,7 +31,23 @@ const updateBikeInfoIntoDB = async (id: string, payload: Partial<TBike>) => {
 };
 
 const deleteBikeFromDB = async (bikeId: string) => {
-  const result = await Bike.findByIdAndDelete(bikeId);
+  // Find the bike by its ID
+  const bike = await Bike.findById(bikeId);
+
+  // Check if the bike is found
+  if (!bike) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Bike not found');
+  }
+
+  // Check if the bike is not available
+  if (bike.isAvailable === false) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Bike is already unavailable');
+  }
+  const result = await Bike.findByIdAndUpdate(
+    bikeId,
+    { isAvailable: false },
+    { new: true },
+  );
 
   return result;
 };
